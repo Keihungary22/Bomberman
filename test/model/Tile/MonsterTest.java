b4fa173fcc90baf16ee85014c99c89792f4c6781
masatonaut <i.masato0907@gmail.com>
2024-04-26 20:25:28 +0200

    add monster test cases

---
+++ test/model/Tile/MonsterTest.java
@@ -0,0 +1,49 @@
+package model.Tile;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MonsterTest {
+    private Monster monster;
+
+    @BeforeEach
+    void setUp() {
+        monster = new Monster("basicMonster.png");
+    }
+
+    @Test
+    void testIsAlive() {
+        assertTrue(monster.isAlive(), "Monster should initially be alive.");
+    }
+
+    @Test
+    void testUpdateMovement() {
+        Monster.Direction originalDirection = monster.getDirection();
+        for (int i = 0; i < 10; i++) {
+            monster.updateMovement();
+        }
+        assertNotEquals(originalDirection, monster.getDirection(), "Monster direction should have changed.");
+    }
+
+    @Test
+    void testInteractWithMonster() {
+        Monster anotherMonster = new Monster("anotherMonster.png");
+        monster.interact(anotherMonster);
+        assertNotEquals(Monster.Direction.STOP, monster.getDirection(), "Monster should change direction after interacting with another monster.");
+        assertNotEquals(Monster.Direction.STOP, anotherMonster.getDirection(), "The other monster should also change direction.");
+    }
+
+    @Test
+    void testSetAndGetDirection() {
+        monster.setDirection(Monster.Direction.LEFT);
+        assertEquals(Monster.Direction.LEFT, monster.getDirection(), "Monster direction should be set to LEFT.");
+    }
+
+    @Test
+    void testSetAndGetSpeed() {
+        monster.setSpeed(2);
+        assertEquals(2, monster.getSpeed(), "Monster speed should be set to 2.");
+    }
+}
